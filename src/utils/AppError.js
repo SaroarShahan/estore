3e1b7541aa1b3d87ac36386fb4645d084e85f8e9
SaroarShahan <saroar.shahan@gmail.com>
2026-09-13 18:12:46 +0600

    feat: add utils

---
+++ src/utils/AppError.js
@@ -0,0 +1,24 @@
+const { httpStatus } = require('./../constants/HttpStatusCode');
+
+export default class AppError extends Error {
+  isOperational;
+  message;
+  errorSource;
+  additionalData;
+  constructor(
+    _message = '',
+    errorSource = 'system',
+    additionalData = {},
+    _isOperational = true,
+    _httpStatusCode = httpStatus.internalServerError,
+  ) {
+    super();
+    Error.call(this);
+    Error.captureStackTrace(this);
+    this.message = _message;
+    this.isOperational = _isOperational;
+    this.httpStatusCode = _httpStatusCode;
+    this.errorSource = errorSource;
+    this.additionalData = additionalData;
+  }
+}
